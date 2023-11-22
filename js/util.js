dc7ce02dbaa73852f2508238a95e2b68fb8934bd
Danil <ashiev_danil@rambler.ru>
2023-11-22 15:36:59 +0500

    opening  image

--- js/util.js
+++ js/util.js
@@ -1,20 +1,18 @@
 const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a,b));
-  const upper = Math.floor(Math.max(a,b));
-  const result = Math.random() * (upper - lower + 1) + lower;
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() *
+    (
+      upper - lower + 1
+    ) +
+    lower;
   return Math.floor(result);
 };
 
-const getRandomArrayElement = (items) =>
-  items[getRandomInteger(0, items.length - 1)];
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const createIdGenerator = () => {
-  let lastGeneratedId = 0;
+const isEscapeKey = (evt) => evt.key === 'Escape';
 
-  return () => {
-    lastGeneratedId += 1;
-    return lastGeneratedId;
-  };
-};
+const isEnterKey = (evt) => evt.key === 'Enter';
 
-export { getRandomInteger, getRandomArrayElement, createIdGenerator };
+export {getRandomArrayElement, isEscapeKey, isEnterKey };
